903e8acf7700cb47f834a0f540821cc573447d1b
Janos Gyerik <janos@kronos>
2016-08-14 21:19:09 +0200

    Add Task admin with custom field list

--- sptt/tasks/admin.py
+++ sptt/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'period', 'name', 'count')
+
+
+admin.site.register(Task, TaskAdmin)
