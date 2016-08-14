0589100d72b8623e984ef920d571019642737bb3
Janos Gyerik <janos@kronos>
2016-08-14 22:14:12 +0200

    Add link to task from task list on admin

--- sptt/tasks/admin.py
+++ sptt/tasks/admin.py
@@ -4,6 +4,7 @@ from tasks.models import Task
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('user', 'period', 'name', 'count')
+    list_display_links = ('name',)
 
 
 admin.site.register(Task, TaskAdmin)
