a217f29c24c722a29224ee46ea8e5894995b8783
Janos Gyerik <janos@kronos>
2016-08-14 22:44:46 +0200

    Implement mark_done

--- sptt/tasks/urls.py
+++ sptt/tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^pending$', views.pending_tasks, name='pending_tasks'),
+    url(r'^actions/mark-done/(?P<task_id>\d+)$', views.mark_done, name='mark_done'),
 ]
--- sptt/tasks/views.py
+++ sptt/tasks/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tasks import api
 from tasks import periods
+from tasks.models import Task
 
 
 @login_required
@@ -14,3 +15,9 @@ def pending_tasks(request):
         'weekly_list': weekly_list,
     }
     return render(request, 'tasks/pending.html', context)
+
+
+@login_required
+def mark_done(request, task_id):
+    api.record_task(request.user, Task.objects.get(pk=task_id))
+    return redirect('pending_tasks')
