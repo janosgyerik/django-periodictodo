2909b24e87b3b8783071337a17eac763056fa4c4
Janos Gyerik <janos@kronos>
2016-08-14 22:32:55 +0200

    Add admin for TaskRecord

--- sptt/tasks/admin.py
+++ sptt/tasks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from tasks.models import Task
+from tasks.models import Task, TaskRecord
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class TaskAdmin(admin.ModelAdmin):
     list_display_links = ('name',)
 
 
+class TaskRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'task', 'created_at')
+    list_display_links = ('created_at',)
+
+
 admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskRecord, TaskRecordAdmin)
