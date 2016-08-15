5b624e48a70cfb06065dd0f3b18aaaf9fcf1c845
Janos Gyerik <janos@kronos>
2016-08-15 10:48:56 +0200

    Rename sptt to periodictodo

--- periodictodo/periodictodo/wsgi.py
+++ periodictodo/periodictodo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sptt project.
+WSGI config for periodictodo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sptt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "periodictodo.settings")
 
 application = get_wsgi_application()
