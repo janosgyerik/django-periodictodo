fdc0ce7c5b7046b14f73e3a5abc860fc37d74168
Janos Gyerik <janos@kronos>
2016-08-15 11:17:16 +0200

    Rename pending_tasks to task_list

--- periodictodo/tasks/urls.py
+++ periodictodo/tasks/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^pending$', views.pending_tasks, name='pending_tasks'),
+    url(r'^list$', views.task_list, name='task_list'),
     url(r'^actions/mark-done/(?P<task_id>\d+)$', views.mark_done, name='mark_done'),
 ]
--- periodictodo/tasks/views.py
+++ periodictodo/tasks/views.py
@@ -18,7 +18,7 @@ def add_pending_done(progress_list):
 
 
 @login_required
-def pending_tasks(request):
+def task_list(request):
     daily_list = add_pending_done(api.load_progress(request.user, periods.DAILY))
     weekly_list = add_pending_done(api.load_progress(request.user, periods.WEEKLY))
 
@@ -26,10 +26,10 @@ def pending_tasks(request):
         'daily_list': daily_list,
         'weekly_list': weekly_list,
     }
-    return render(request, 'tasks/pending.html', context)
+    return render(request, 'tasks/index.html', context)
 
 
 @login_required
 def mark_done(request, task_id):
     api.record_task(request.user, Task.objects.get(pk=task_id))
-    return redirect('pending_tasks')
+    return redirect('task_list')
