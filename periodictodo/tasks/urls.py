da25f21735f4c1d0d1025a0ab8aee2bd6ac13df7
Janos Gyerik <janos@kronos>
2016-09-03 23:19:37 +0200

    Make both list/ and list work

--- periodictodo/tasks/urls.py
+++ periodictodo/tasks/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^list$', views.task_list, name='task_list'),
+    url(r'^list/$', views.task_list, name='task_list'),
     url(r'^actions/mark-done/(?P<task_id>\d+)$', views.mark_done, name='mark_done'),
 ]
